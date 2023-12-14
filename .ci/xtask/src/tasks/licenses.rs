5fddb99f4c5b342aac2224201fa7fe62be680ead
Frank Märkle <Frank.Maerkle@mercedes-benz.com>
2023-12-14 17:06:03 +0100

    CI -> Introduce `cargo ci licenses sbom`.

--- .ci/xtask/src/tasks/licenses.rs
+++ .ci/xtask/src/tasks/licenses.rs
@@ -3,19 +3,25 @@ use std::path::PathBuf;
 use std::process::Command;
 
 use clap::Subcommand;
-use strum::IntoEnumIterator;
 
 use crate::{constants, util};
 use crate::Package;
+use crate::types::package::PackageSelection;
 use crate::util::RunRequiringSuccess;
 
 #[derive(Debug, Subcommand)]
 pub enum LicensesTask {
+    /// Check for license violations and security vulnerabilities
     Check,
-    /// Generate a license representation in JSON
+    /// Generate a license report in JSON format
     Json {
-        #[arg(long)]
-        package: Option<Package>,
+        #[arg(long, default_value_t)]
+        package: PackageSelection,
+    },
+    /// Generate a license report in SBOM format
+    Sbom {
+        #[arg(long, default_value_t)]
+        package: PackageSelection,
     },
 }
 impl LicensesTask {
@@ -25,12 +31,14 @@ impl LicensesTask {
             LicensesTask::Check => {
                 check::check_licenses()?;
             }
-            LicensesTask::Json { package } => match package {
-                Some(package) => json::export_json(&package)?,
-                None => {
-                    for package in Package::iter() {
-                        json::export_json(&package)?
-                    }
+            LicensesTask::Json { package } => {
+                for package in package.iter() {
+                    json::export_json(&package)?
+                }
+            }
+            LicensesTask::Sbom { package } => {
+                for package in package.iter() {
+                    sbom::generate_sbom(&package)?
                 }
             }
         };
@@ -77,5 +85,154 @@ pub mod json {
         constants::target_dir()
             .join("licenses")
             .join(format!("{}.licenses.json", package.ident()))
+    }
+}
+
+mod sbom {
+    use super::*;
+
+    #[tracing::instrument]
+    pub fn generate_sbom(package: &Package) -> anyhow::Result<()> {
+        //! [dependencies]
+        //! serde = { version = "*", features = ["derive"] } #only needed due to compile error in serde-spdx v0.8.4
+        //! ``` TODO needed?
+        use serde_spdx::spdx::v_2_3::{Spdx, SpdxItemPackages};
+
+        util::install_crate("cargo-sbom")?;
+
+
+        let sbom_dir = out_dir();
+        fs::create_dir_all(&sbom_dir)?;
+
+        let sbom = Command::new("cargo-sbom")
+            .args(["--cargo-package", &package.ident(), "--output-format", "spdx_json_2_3"])
+            .output()?
+            .stdout;
+        let sbom = std::str::from_utf8(&sbom)?;
+
+        let mut sbom: Spdx = serde_json::from_str(&sbom)?;
+
+        { //override license information for crates with unclear license
+            sbom.packages = sbom.packages.map(|packages|
+                packages.into_iter().map(|package| {
+                    if package.name == "ring" {
+                        SpdxItemPackages {
+                            license_concluded: Some(String::from("MIT AND ISC AND OpenSSL")), //comply with all licenses to be on the safe side
+                            license_declared: Some(String::from("NOASSERTION")),
+                            ..package
+                        }
+                    } else {
+                        let license_concluded = package.license_concluded.as_ref().map(|license|
+                            // When selecting a license, choose Apache-2.0 where possible.
+                            // Otherwise, select the most permissive license.
+
+                            match license.as_ref() {
+                                "Apache-2.0 OR MIT"
+                                | "Apache-2.0 OR Apache-2.0 OR MIT"
+                                | "Apache-2.0 OR BSL-1.0"
+                                | "Apache-2.0 OR ISC OR MIT"
+                                | "MIT OR Apache-2.0"
+                                | "MIT OR Apache-2.0 OR Zlib"
+                                | "MIT OR Zlib OR Apache-2.0"
+                                | "Zlib OR Apache-2.0 OR MIT"
+                                | "0BSD OR MIT OR Apache-2.0"
+                                => "Apache-2.0",
+
+                                "BSD-3-Clause OR MIT" => "BSD-3-Clause",
+                                "Unlicense OR MIT" => "MIT",
+                                "(Apache-2.0 OR MIT) AND BSD-3-Clause" => "Apache-2.0 AND BSD-3-Clause",
+                                "(MIT OR Apache-2.0) AND Unicode-DFS-2016" => "Apache-2.0 AND Unicode-DFS-2016",
+
+                                "Apache-2.0"
+                                | "BSD-3-Clause"
+                                | "BSL-1.0"
+                                | "ISC"
+                                | "MIT"
+                                | "MIT AND Apache-2.0"
+                                | "MIT AND BSD-3-Clause"
+                                | "Zlib"
+                                | "MPL-2.0"
+                                => license, //leave unchanged
+
+                                other => panic!("Unknown license '{}' for package '{}'. Please define a mapping.", other, package.name)
+                            }.to_string()
+                        );
+
+                        let license_declared = package.license_declared.as_ref().map(|license|
+                            // Change slashes into "OR" to improve compatibility with external systems.
+
+                            if license.contains('/') {
+
+                                match license.as_ref() {
+                                    "Apache-2.0/MIT"
+                                    | "MIT/Apache-2.0"
+                                    | "MIT / Apache-2.0"
+                                    | "Apache-2.0 / MIT"
+                                    => "Apache-2.0 OR MIT",
+
+                                    "BSD-3-Clause/MIT"
+                                    | "MIT/BSD-3-Clause"
+                                    | "MIT / BSD-3-Clause"
+                                    | "BSD-3-Clause / MIT"
+                                    => "BSD-3-Clause OR MIT",
+
+                                    "Unlicense/MIT"
+                                    | "MIT/Unlicense"
+                                    | "MIT / Unlicense"
+                                    | "Unlicense / MIT"
+                                    => "MIT OR Unlicense",
+
+                                    "Apache-2.0/ISC/MIT"
+                                    | "Apache-2.0 / ISC / MIT"
+                                    | "Apache-2.0/MIT/ISC"
+                                    | "Apache-2.0 / MIT / ISC"
+                                    | "ISC/Apache-2.0/MIT"
+                                    | "ISC / Apache-2.0 / MIT"
+                                    | "ISC/MIT/Apache-2.0"
+                                    | "ISC / MIT / Apache-2.0"
+                                    | "MIT/ISC/Apache-2.0"
+                                    | "MIT / Apache-2.0 / ISC"
+                                    | "MIT/Apache-2.0/ISC"
+                                    | "MIT / ISC / Apache-2.0"
+                                    => "MIT OR ISC OR Apache-2.0",
+
+                                    other => panic!("Unmatched license specification '{}' for package '{}'. Please check mapping.", other, package.name)
+                                }.to_string()
+                            }
+                            else {
+                                license.to_string()
+                            }
+                        );
+                        SpdxItemPackages {
+                            license_concluded,
+                            license_declared,
+                            ..package
+                        }
+                    }
+                }).collect::<Vec<_>>()
+            );
+        }
+
+        let sbom = serde_json::to_string_pretty(&sbom)?;
+
+        fs::write(
+            sbom_dir.join(format!("{}-sbom.spdx.json", package.ident())),
+            sbom
+        )?;
+
+        Ok(())
+    }
+
+    #[tracing::instrument]
+    fn clean() -> anyhow::Result<()> {
+        let sbom_dir = out_dir();
+        if sbom_dir.exists() {
+            fs::remove_dir_all(sbom_dir)?;
+        }
+        Ok(())
+    }
+
+    pub fn out_dir() -> PathBuf {
+        constants::target_dir().join("sbom")
     }
 }
--- .ci/xtask/src/types/package.rs
+++ .ci/xtask/src/types/package.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+use std::iter;
 use clap::builder::PossibleValue;
 use strum::IntoEnumIterator;
 
@@ -34,18 +36,51 @@ impl Package {
     }
 }
 
-impl std::fmt::Display for Package {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl Display for Package {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.ident())
     }
 }
 
 
-impl clap::ValueEnum for Package {
-    fn value_variants<'a>() -> &'a [Package] {
-        Box::leak(Self::iter().collect::<Vec<Package>>().into())
-    }
-    fn to_possible_value(&self) -> Option<PossibleValue> {
-        Some(PossibleValue::new(self.ident()))
+#[derive(Clone, Debug, Default)]
+pub enum PackageSelection {
+    #[default]
+    All,
+    Single(Package),
+}
+const PACKAGE_SELECTION_ALL: &str = "all";
+impl PackageSelection {
+    pub fn iter(&self) -> Box<dyn Iterator<Item=Package>> {
+        match self {
+            PackageSelection::Single(package) => Box::new(iter::once(Clone::clone(package))),
+            PackageSelection::All => Box::new(Package::iter()),
+        }
     }
 }
+impl Display for PackageSelection {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PackageSelection::Single(package) => write!(f, "{}", package),
+            PackageSelection::All => write!(f, "{}", PACKAGE_SELECTION_ALL),
+        }
+    }
+}
+
+impl clap::ValueEnum for PackageSelection {
+    fn value_variants<'a>() -> &'a [PackageSelection] {
+        let variants = Package::iter()
+            .map(PackageSelection::Single)
+            .chain(iter::once(PackageSelection::All))
+            .collect::<Vec<PackageSelection>>();
+
+        Box::leak(variants.into())
+    }
+    fn to_possible_value(&self) -> Option<PossibleValue> {
+        match self {
+            PackageSelection::Single(package) => Some(PossibleValue::new(package.ident())),
+            PackageSelection::All => Some(PossibleValue::new(PACKAGE_SELECTION_ALL)),
+        }
+    }
+}
+
