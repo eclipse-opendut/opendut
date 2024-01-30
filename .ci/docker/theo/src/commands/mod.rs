78378dfa2a5e6df3da960d2e86fc9da83af7a430
Frank Märkle <Frank.Maerkle@mercedes-benz.com>
2024-01-30 15:51:29 +0100

    THEO -> Initial command structure to have THEO called from within the containers.

--- .ci/docker/theo/src/commands/mod.rs
+++ .ci/docker/theo/src/commands/mod.rs
@@ -2,3 +2,4 @@ pub(crate) mod testenv;
 pub(crate) mod edgar;
 pub(crate) mod vagrant;
 pub(crate) mod dev;
+pub(crate) mod inside_edgar_container;
--- .ci/docker/theo/src/main.rs
+++ .ci/docker/theo/src/main.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
-
 use clap::{Parser, Subcommand};
+use commands::inside_edgar_container;
+use commands::inside_edgar_container::InsideEdgarContainerCli;
 
 use crate::commands::dev::DevCli;
 use crate::commands::testenv::TestenvCli;
@@ -27,14 +28,23 @@ enum TaskCli {
     Dev(DevCli),
     /// Show NetBird application versions.
     NetbirdVersions,
+
+    InsideEdgarContainer(InsideEdgarContainerCli),
 }
 
 
 fn main() -> Result {
     let args = Cli::parse();
-    PathBuf::project_dir_verify();
 
-    project::load_environment_variables_from_dot_env_file();
+    match args.command {
+        TaskCli::InsideEdgarContainer(_) => {
+            /* Don't set up repo-derived variables, since we don't have the repo mounted into the containers. */
+        },
+        _ => {
+            PathBuf::project_dir_verify();
+            project::load_environment_variables_from_dot_env_file();
+        }
+    };
 
     match args.command {
         TaskCli::Testenv(implementation) => { implementation.default_handling()? }
@@ -44,6 +54,10 @@ fn main() -> Result {
             let metadata = core::metadata::cargo_netbird_versions();
             println!("Versions: {:?}", metadata);
         }
-    }
+
+        TaskCli::InsideEdgarContainer(cli) => {
+            inside_edgar_container::call_managed_sh(cli.task)?;
+        }
+    };
     Ok(())
 }
