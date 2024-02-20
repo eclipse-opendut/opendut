f93cd1a5bdd75fc0c644b251d7bb0df1b0f02120
Frank Märkle <Frank.Maerkle@mercedes-benz.com>
2024-02-20 17:27:07 +0100

    EDGAR Setup -> Fix user-exists check erronously accepting similarly named users.

--- opendut-edgar/src/setup/tasks/create_user.rs
+++ opendut-edgar/src/setup/tasks/create_user.rs
@@ -17,22 +17,30 @@ impl Task for CreateUser {
     fn description(&self) -> String {
         format!("Create User \"{}\"", self.service_user.name)
     }
+
     fn check_fulfilled(&self) -> Result<TaskFulfilled> {
         let passwd = fs::read_to_string(passwd_file())?;
+
         let user_exists = passwd.lines()
-            .any(|line| line.starts_with(&self.service_user.name));
+            .any(|line| match line.split(':').next() {
+                Some(user) => user == self.service_user.name,
+                None => false,
+            });
+
         if user_exists {
             Ok(TaskFulfilled::Yes)
         } else {
             Ok(TaskFulfilled::No)
         }
     }
+
     fn execute(&self) -> Result<Success> {
         Command::new("useradd")
             .arg("--no-create-home")
             .arg(&self.service_user.name)
             .evaluate_requiring_success()
             .context("Error when trying to create user '{USER_NAME}'.")?;
+
         Ok(Success::default())
     }
 }
