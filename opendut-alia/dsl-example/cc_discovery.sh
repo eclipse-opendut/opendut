3428050c14d4ef30ddf48acc0f648414ad6f045d
smarksteiner <110222996+smarksteiner@users.noreply.github.com>
2024-04-10 08:15:33 +0200

    Add files via upload

---
+++ opendut-alia/dsl-example/cc_discovery.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+usage() { echo "Usage: $0 [-i <CAN_INTERFACE>]" 1>&2; exit 1;}
+
+# arg1 = CAN_INTERFACE
+CAN_INTERFACE=""
+
+while getopts ":i:" opt; do
+  case "$opt" in
+    i)
+      CAN_INTERFACE=${OPTARG}
+      ;;
+    :)                                    
+      echo "Error: ${OPTARG} argument missing."    
+      usage
+      exit 1                   
+      ;;  
+    *)
+      usage
+      exit 1
+      ;;
+    \?)
+      echo "Error: Syntax Invalid"
+      usage
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+#if [ -z "${CAN_INTERRACE}"]; then
+# usage
+#fi
+
+# Redirect to path
+cd /home/kali/Documents/CAN/CC/caringcaribou/
+
+# Get Arbitration IDs from discovery
+cc.py -i $CAN_INTERFACE uds discovery
+
+echo "Finished execution!"
