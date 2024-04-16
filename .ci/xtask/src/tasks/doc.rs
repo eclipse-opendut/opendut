98a5ab27b236e70e8b3c77a5eeaf7f869a4d1391
Matthias Twardawski <matthias.twardawski@mercedes-benz.com>
2024-04-16 13:27:23 +0200

    DOC: Issue-118 - Added build and deployment process of homepage to a GitHub actions based workflow, with no need to handle a new branch and push changes to it.

--- .ci/xtask/src/tasks/doc.rs
+++ .ci/xtask/src/tasks/doc.rs
@@ -18,6 +18,11 @@ enum DocKindCli {
         #[command(subcommand)]
         task: BookCli,
     },
+    /// Build and pack homepage for openDuT
+    Homepage {
+        #[command(subcommand)]
+        task: HomepageCli,
+    },
 }
 #[derive(Debug, clap::Subcommand)]
 enum BookCli {
@@ -27,13 +32,22 @@ enum BookCli {
     Open,
 }
 
+#[derive(Debug, clap::Subcommand)]
+enum HomepageCli {
+    /// Build the homepage
+    Build,
+}
+
 impl DocCli {
     pub fn default_handling(&self) -> crate::Result {
         match &self.kind {
             DocKindCli::Book { task } => match task {
                 BookCli::Build => book::build()?,
                 BookCli::Open => book::open()?,
-            }
+            },
+            DocKindCli::Homepage { task } => match task {
+                HomepageCli::Build => homepage::build()?,
+            },
         };
         Ok(())
     }
@@ -83,4 +97,58 @@ pub mod book {
     fn out_dir() -> PathBuf {
         crate::constants::target_dir().join("book")
     }
+}
+
+pub mod homepage {
+    use tracing::info;
+    use super::*;
+
+    #[tracing::instrument]
+    pub fn build() -> crate::Result {
+
+        Command::new("mdbook")
+            .arg("build")
+            .arg("--dest-dir").arg(&out_dir().join("book"))
+            .current_dir(doc_dir())
+            .run_requiring_success()?;
+
+
+        fs_extra::dir::copy(
+            homepage_source_dir(),
+            out_dir(),
+            &fs_extra::dir::CopyOptions::default()
+                .overwrite(true)
+                .content_only(true)
+        )?;
+
+        fs_extra::dir::create(
+            logos_out_dir(),
+            true
+        )?;
+
+        for logo in RESOURCES_TO_INCLUDE {
+            fs_extra::file::copy(
+                &logos_source_dir().join(logo),
+                &logos_out_dir().join(logo),
+                &fs_extra::file::CopyOptions::default()
+                    .overwrite(true)
+            )?;
+        }
+
+        info!("Placed distribution into: {}", out_dir().display());
+
+        Ok(())
+    }
+
+    fn homepage_source_dir() -> PathBuf { crate::constants::workspace_dir().join("opendut-homepage") }
+
+    fn logos_source_dir() -> PathBuf { crate::constants::workspace_dir().join("resources").join("logos") }
+
+    fn doc_dir() -> PathBuf { crate::constants::workspace_dir().join("doc") }
+
+    fn out_dir() -> PathBuf { crate::constants::target_dir().join("homepage") }
+
+    fn logos_out_dir() -> PathBuf { out_dir().join("resources/logos") }
+
+    const RESOURCES_TO_INCLUDE: [&str; 2] = ["logo_light.png", "funded_by_the_european_union.svg"];
 }
