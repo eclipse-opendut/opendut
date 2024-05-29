5e5cbcaa8fd4f83f9c81da3999dd953dfb9a6f18
Frank Märkle <Frank.Maerkle@mercedes-benz.com>
2024-05-29 14:32:56 +0200

    CI xtask -> Output tracing on stderr.
    This allows piping the regular program output, e.g. from `cargo cleo`.

--- .ci/xtask/src/core/util.rs
+++ .ci/xtask/src/core/util.rs
@@ -65,6 +65,7 @@ pub fn init_tracing() -> crate::Result {
         .with_max_level(tracing::Level::TRACE)
         .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
         .with_env_filter(tracing_filter)
+        .with_writer(std::io::stderr)
         .compact()
         .init();
     Ok(())
