6615bf9a0ba8f30f32d93ee6a6bb19bda0d8951d
Martin Heyden <mrh000@protonmail.com>
2024-07-11 08:09:08 -0400

    added some comments to main.rs of Restbus Simulation

--- opendut-edgar/restbus-simulation/src/main.rs
+++ opendut-edgar/restbus-simulation/src/main.rs
@@ -18,6 +18,9 @@ use restbus_simulation::*;
 use restbus_structs::TimedCanFrame;
 
 
+/*
+    Debug method. Combines the CAN ID and intial values as hex of a PDU. Returns this as string.
+*/
 fn get_pdu_hex(can_id: &u64, init_values: &Vec<u8>) -> String {
     let mut hex_string = String::new();
     hex_string.push_str(&format!("{}:", can_id));
@@ -29,6 +32,10 @@ fn get_pdu_hex(can_id: &u64, init_values: &Vec<u8>) -> String {
     return hex_string;
 }
 
+
+/*
+    Debug method. Iterates over all PDUs of the target bus and returns all combinations of CAN IDs and initial values as string.
+*/
 fn collect_pdus(can_clusters: &HashMap<String, CanCluster>, bus_name: String) -> Vec<String> {
     let mut init_values_strings: Vec<String> = Vec::new();
     if let Some(can_cluster) = can_clusters.get(&bus_name) {
@@ -58,6 +65,10 @@ fn collect_pdus(can_clusters: &HashMap<String, CanCluster>, bus_name: String) ->
     return init_values_strings;
 }
 
+
+/*
+    Play a single CAN frame from the target bus periodcically (if it is periodic) to the bus to which ifname is connected to.
+*/
 fn test_find_frame_and_play(restbus_simulation: &RestbusSimulation, ifname: &String, can_clusters: &HashMap<String, CanCluster>, bus_name: String, can_id: u64) {
     let timed_can_frames: Vec<TimedCanFrame> = get_timed_can_frame_from_id(can_clusters, bus_name, can_id);
 
@@ -67,23 +78,34 @@ fn test_find_frame_and_play(restbus_simulation: &RestbusSimulation, ifname: &Str
     }
 
 }
-    
+
+
+/*
+    Play all CAN frames from the target bus periodcically (only periodic frames are sent periodically) to the bus to which ifname is connected to.
+*/
 fn test_bus_play_all(restbus_simulation: &RestbusSimulation, ifname: &String, can_clusters: &HashMap<String, CanCluster>, bus_name: String) {
     let timed_can_frames: Vec<TimedCanFrame> = get_timed_can_frames_from_bus(can_clusters, bus_name);
 
     match restbus_simulation.play_all(&timed_can_frames, ifname) {
         Ok(_val) => println!("Successfully established restbus simulation"),
-        Err(error) => println!("Could establish restbus simulation because: {}", error)
+        Err(error) => println!("Could not establish restbus simulation because: {}", error)
     }
 }
 
+
+/*
+    This shows an example of how to create a restbus-simulation with an ARXML file.
+*/ 
 fn main() -> std::io::Result<()> {
     println!("[+] Starting openDuT ARXML parser over main method.");
-   
+
+    // Define file name. It is used as a path.
     let file_name = "system-4.2.arxml"; // from https://github.com/cantools/cantools/blob/master/tests/files/arxml/system-4.2.arxml
 
+    // Create ArxmlParser struct, which contains all the parsing methods.
     let arxml_parser: ArxmlParser = ArxmlParser {};
 
+    // Parse the ARXML file. Use serialized file if it exists. Parsed data is stored in can cluster ARXML element representations.
     if let Some(can_clusters) = arxml_parser
         .parse_file(&file_name.to_string(), true) 
     {
@@ -91,32 +113,39 @@ fn main() -> std::io::Result<()> {
         let target_file = "system.txt";
         let play_single_or_all = true;
         let ifname = String::from("vcan0");
-        
+
+        // Debug output 
         for cluster in can_clusters.values() {
             println!("CanCluster: {cluster:?}");
         }
-
+         
+        // Debug output 
         let mut frames = String::new();
         for frame in collect_pdus(&can_clusters, String::from(bus_name)) {
             frames.push_str(frame.as_str());
             frames.push_str("\n");
         }
 
+        // Debug. Found PDUs of target bus are written to file.
         let mut f = File::create(target_file)?;
         f.write_all(&frames.as_bytes())?;
 
         println!("Trying to setup up restbus simulation");
 
+        // Create RestbusSimulation structure, which contains all the relevant methods for the restbus simulation.
         let restbus_simulation: RestbusSimulation = RestbusSimulation {};
 
         if !play_single_or_all {
+            // Play a single CAN frame from the target bus periodcically (if it is periodic) to the bus to which ifname is connected to.
             test_find_frame_and_play(&restbus_simulation, &ifname, &can_clusters, String::from(bus_name), 0x3E9);
 
         } else {
+            // Play all CAN frames from the target bus periodcically (only periodic frames are sent periodically) to the bus to which ifname is connected to.
             test_bus_play_all(&restbus_simulation, &ifname, &can_clusters, String::from(bus_name));
         }
 
-        thread::sleep(Duration::from_secs(30));
+        // Sleep as long as restbus-simulation should run.
+        thread::sleep(Duration::from_secs(300));
     }
     Ok(())
 }
