6c57632eee5fe3c4dbcb4bd385b0d43e45e1aef3
Frank Märkle <Frank.Maerkle@mercedes-benz.com>
2024-09-19 10:14:58 +0200

    Integration tests -> Use backoff for retrying.

--- tests/src/register.rs
+++ tests/src/register.rs
@@ -1,5 +1,6 @@
-use std::time::Duration;
+use anyhow::anyhow;
 use opendut_util::telemetry;
+use std::ops::Not;
 
 use crate::util;
 
@@ -11,22 +12,20 @@ async fn register_edgar_carl() -> anyhow::Result<()> {
 
     util::spawn_edgar(carl_port)?;
 
-    let mut carl_client = util::spawn_carl_client(carl_port).await?;
+    let carl_client = util::spawn_carl_client(carl_port).await?;
 
-    let retries = 5;
-    let interval = Duration::from_millis(500);
-    for retries_left in (0..retries).rev() {
-        let peers = carl_client.broker.list_peers().await?;
+    util::retry(|| async {
+        let mut carl_client = carl_client.lock().await;
 
-        if peers.is_empty() {
-            if retries_left > 0 {
-                tokio::time::sleep(interval).await;
-            } else {
-                panic!("EDGAR did not register within {retries}*{} ms!", interval.as_millis());
-            }
+        let peers = carl_client.broker.list_peers().await
+            .map_err(|cause| backoff::Error::transient(cause.into()))?;
+
+        if peers.is_empty().not() {
+            Ok(())
         } else {
-            return Ok(());
+            Err(backoff::Error::transient(anyhow!("No peers registered in time!")))
         }
-    }
+    }).await?;
+
     Ok(())
 }
--- tests/src/util.rs
+++ tests/src/util.rs
@@ -1,9 +1,12 @@
-use tracing::info;
 use opendut_carl_api::carl::CarlClient;
 use opendut_edgar::service::start::DefaultPeerConfigurationApplier;
 use opendut_types::peer::PeerId;
 use opendut_types::util::Port;
 use opendut_util::settings::LoadedConfig;
+use std::future::Future;
+use std::time::Duration;
+use tokio::sync::Mutex;
+use tracing::info;
 
 pub fn spawn_carl() -> anyhow::Result<Port> {
     let carl_port = select_free_port();
@@ -42,15 +45,26 @@ pub fn spawn_edgar(carl_port: Port) -> anyhow::Result<()> {
     Ok(())
 }
 
-pub async fn spawn_carl_client(carl_port: Port) -> anyhow::Result<CarlClient> {
+pub async fn spawn_carl_client(carl_port: Port) -> anyhow::Result<Mutex<CarlClient>> {
     let peer_id = PeerId::random();
 
     let edgar_config = load_edgar_config(carl_port, peer_id)?;
 
     let carl_client = opendut_edgar::common::carl::connect(&edgar_config.config).await
         .expect("Failed to connect to CARL for state checks");
-        
-    Ok(carl_client)
+
+    Ok(Mutex::new(carl_client))
+}
+
+pub async fn retry<T, Fut>(assertion: impl FnMut() -> Fut) -> anyhow::Result<T>
+where
+    Fut: Future<Output=Result<T, backoff::Error<anyhow::Error>>> + Sized
+{
+    let backoff_policy = backoff::ExponentialBackoffBuilder::new()
+        .with_max_elapsed_time(Some(Duration::from_secs(15)))
+        .build();
+
+    backoff::future::retry(backoff_policy, assertion).await
 }
 
 fn load_edgar_config(carl_port: Port, peer_id: PeerId) -> anyhow::Result<LoadedConfig> {
