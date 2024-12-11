2741e20d7451a027a0bfe19b4a870772a8ed43e9
vv-ab <vivianberger028@gmail.com>
2024-12-11 15:27:10 +0100

    CLEO -> Set argument output as globally usable.

--- opendut-cleo/src/main.rs
+++ opendut-cleo/src/main.rs
@@ -37,7 +37,7 @@ enum Commands {
         #[command(subcommand)]
         resource: ListResource,
         ///JSON, prettified JSON or table as output format
-        #[arg(value_enum, short, long, default_value_t=ListOutputFormat::Table)]
+        #[arg(global = true, value_enum, short, long, default_value_t=ListOutputFormat::Table)]
         output: ListOutputFormat,
     },
     /// Create openDuT resources from specifications files
@@ -47,7 +47,7 @@ enum Commands {
         #[command(subcommand)]
         resource: Box<CreateResource>,
         ///Text, JSON or prettified JSON as output format
-        #[arg(value_enum, short, long, default_value_t=CreateOutputFormat::Text)]
+        #[arg(global = true, value_enum, short, long, default_value_t=CreateOutputFormat::Text)]
         output: CreateOutputFormat,
     },
     GenerateSetupString(commands::generate_setup_string::GenerateSetupStringCli),
@@ -58,7 +58,7 @@ enum Commands {
         #[command(subcommand)]
         resource: DescribeResource,
         ///JSON, prettified JSON or table as output format
-        #[arg(value_enum, short, long, default_value_t=DescribeOutputFormat::Text)]
+        #[arg(global = true, value_enum, short, long, default_value_t=DescribeOutputFormat::Text)]
         output: DescribeOutputFormat,
     },
     ///Find openDuT resource
@@ -67,7 +67,7 @@ enum Commands {
         #[command(subcommand)]
         resource: FindResource,
         ///JSON, prettified JSON or table as output format
-        #[arg(value_enum, short, long, default_value_t=ListOutputFormat::Table)]
+        #[arg(global = true, value_enum, short, long, default_value_t=ListOutputFormat::Table)]
         output: ListOutputFormat,
     },
     ///Delete openDuT resource
