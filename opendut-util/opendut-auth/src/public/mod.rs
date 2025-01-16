58222c303cf542bb82a1c8e58f3de77661071ef9
Frank Märkle <Frank.Maerkle@mercedes-benz.com>
2025-01-16 14:56:22 +0100

    WIP: LEA AuthInterceptor -> Properly handle when auth is disabled.

--- opendut-util/opendut-auth/src/public/mod.rs
+++ opendut-util/opendut-auth/src/public/mod.rs
@@ -11,25 +11,26 @@ impl AuthInterceptor {
     pub fn new(auth: Option<Auth>) -> Self {
         Self { auth }
     }
-
-    fn access_token(&self) -> Option<String> {
-        self.auth.as_ref().and_then(|auth|auth.access_token())
-    }
 }
 
 impl Interceptor for AuthInterceptor {
     fn call(&mut self, mut request: tonic::Request<()>) -> Result<tonic::Request<()>, Status> {
-        match self.access_token() {
-            None => {
-                tracing::debug!("AuthInterceptor: No access token present.");
-                Err(Status::unauthenticated("No access token present."))
+        match &self.auth {
+            Some(auth) => {
+                match auth.access_token() {
+                    None => {
+                        tracing::debug!("AuthInterceptor: No access token present.");
+                        Err(Status::unauthenticated("No access token present."))
+                    }
+                    Some(token) => {
+                        let bearer_token: tonic::metadata::MetadataValue<_> = format!("Bearer {}", token).parse()
+                            .map_err(|_err| Status::unauthenticated("could not parse token"))?;
+                        request.metadata_mut().insert(http::header::AUTHORIZATION.as_str(), bearer_token);
+                        Ok(request)
+                    }
+                }
             }
-            Some(token) => {
-                let bearer_token: tonic::metadata::MetadataValue<_> = format!("Bearer {}", token).parse()
-                    .map_err(|_err| Status::unauthenticated("could not parse token"))?;
-                request.metadata_mut().insert(http::header::AUTHORIZATION.as_str(), bearer_token);
-                Ok(request)
-            }
+            None => Ok(request)
         }
     }
 }
