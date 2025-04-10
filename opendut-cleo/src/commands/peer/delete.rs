bf791fe0ea4dae6fa44788b726e058ab6e47b51e
Reimar Stier <reimar.stier@mercedes-benz.com>
2025-04-10 17:20:01 +0200

    CLEO -> Use HashSet to collect cluster ids, removing duplicate responses.

--- opendut-cleo/src/commands/peer/delete.rs
+++ opendut-cleo/src/commands/peer/delete.rs
@@ -1,6 +1,8 @@
+use std::collections::HashSet;
 use std::ops::Not;
 
 use opendut_carl_api::carl::CarlClient;
+use opendut_types::cluster::ClusterId;
 use opendut_types::peer::PeerId;
 
 /// Delete a peer
@@ -26,16 +28,17 @@ impl DeletePeerCli {
                 .await
                 .map_err(|error| format!("Failed to list cluster configurations.\n  {}", error))?;
 
-            let mut clusters_with_configured_devices: Vec<String> = vec![];
+            let mut clusters_with_configured_devices: HashSet<ClusterId> = HashSet::new();
             for cluster in clusters {
                 for device in cluster.devices {
                     if peer_device_ids.contains(&device) {
-                        clusters_with_configured_devices.push(cluster.id.to_string());
+                        clusters_with_configured_devices.insert(cluster.id);
                     }
                 }
             }
             if clusters_with_configured_devices.is_empty().not() {
-                Err(format!("Cannot delete peer because it is used in following clusters: {}", clusters_with_configured_devices.join(", ")))?
+                let cluster_list = clusters_with_configured_devices.iter().map(|id| id.to_string()).collect::<Vec<String>>().join(", ");
+                Err(format!("Cannot delete peer because it is used in following clusters: {cluster_list}."))?
             }
         }
         
