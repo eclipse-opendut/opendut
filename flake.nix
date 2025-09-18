a289ee41426dbe8c9e7c432e18d83d72c2db7a12
Elmar Schug <elmar.schug@mercedes-benz.com>
2025-09-18 16:47:47 +0200

    Prepared project for Nix.

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    { ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ (import inputs.rust-overlay) ];
+        };
+        craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (
+          p: p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
+        );
+      in
+      {
+        devShells.default = craneLib.devShell {
+          packages = with pkgs; [
+            bacon
+            cargo-deny
+            clippy
+          ];
+        };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
