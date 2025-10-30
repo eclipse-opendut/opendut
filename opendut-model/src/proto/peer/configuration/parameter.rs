caa57168fac411919fc1c11a27fd59ae829c50e9
Reimar Stier <reimar.stier@mercedes-benz.com>
2025-10-30 15:54:54 +0100

    Model » Move parameters from OldPeerConfiguration.
    - Introduce CAN parameters: CanConnection, CanBridge, CanLocalRoute
    - Refactor parameter conversion
    
    Addresses #412

--- opendut-model/src/proto/peer/configuration/parameter.rs
+++ opendut-model/src/proto/peer/configuration/parameter.rs
@@ -129,3 +129,76 @@ conversion! {
         })
     }
 }
+
+conversion! {
+    type Model = crate::peer::configuration::parameter::CanConnection;
+    type Proto = CanConnection;
+
+    fn from(value: Model) -> Proto {
+        Proto {
+            remote_peer_id: Some(value.remote_peer_id.into()),
+            remote_ip: Some(value.remote_ip.into()),
+            remote_port: Some(value.remote_port.into()),
+            local_port: Some(value.local_port.into()),
+            local_is_server: value.local_is_server,
+            buffer_timeout_microseconds: value.buffer_timeout_microseconds,
+        }
+    }
+
+    fn try_from(value: Proto) -> ConversionResult<Model> {
+        let remote_peer_id = extract!(value.remote_peer_id)?.try_into()?;
+        let remote_ip = extract!(value.remote_ip)?.try_into()?;
+        let remote_port = extract!(value.remote_port)?.try_into()?;
+        let local_port = extract!(value.local_port)?.try_into()?;
+
+        Ok(Model {
+            remote_peer_id,
+            remote_ip,
+            remote_port,
+            local_port,
+            local_is_server: value.local_is_server,
+            buffer_timeout_microseconds: value.buffer_timeout_microseconds,
+        })
+    }
+}
+
+conversion! {
+    type Model = crate::peer::configuration::parameter::CanBridge;
+    type Proto = CanBridge;
+
+    fn from(value: Model) -> Proto {
+        Proto {
+            name: Some(value.name.into()),
+        }
+    }
+
+    fn try_from(value: Proto) -> ConversionResult<Model> {
+        let name = extract!(value.name)?.try_into()?;
+
+        Ok(Model {
+            name,
+        })
+    }
+}
+
+conversion! {
+    type Model = crate::peer::configuration::parameter::CanLocalRoute;
+    type Proto = CanLocalRoute;
+
+    fn from(value: Model) -> Proto {
+        Proto {
+            bridge_name: Some(value.bridge_name.into()),
+            can_device_name: Some(value.can_device_name.into()),
+        }
+    }
+
+    fn try_from(value: Proto) -> ConversionResult<Model> {
+        let bridge_name = extract!(value.bridge_name)?.try_into()?;
+        let can_device_name = extract!(value.can_device_name)?.try_into()?;
+
+        Ok(Model {
+            bridge_name,
+            can_device_name,
+        })
+    }
+}
