37d456612d54e6a99fbb17ab2d980349a0c14f27
Jessica Schach <jessica.schach@mercedes-benz.com>
2025-11-17 14:03:18 +0100

    LEA-Components: Refactored `Tooltip` component and its scss.

--- opendut-lea/opendut-lea-components/src/tooltip.rs
+++ opendut-lea/opendut-lea-components/src/tooltip.rs
@@ -1,16 +1,43 @@
 use leptos::prelude::*;
 
+pub enum TooltipDirection {
+    Left,
+    Right,
+    Up,
+    UpRight,
+}
+
+impl TooltipDirection {
+    fn as_class(&self) -> &'static str {
+        match self {
+            TooltipDirection::Left => "is-left",
+            TooltipDirection::Right => "is-right",
+            TooltipDirection::Up => "is-up",
+            TooltipDirection::UpRight => "is-right is-up",
+        }
+    }
+}
 
 #[component]
 pub fn Tooltip(
-    visible: ReadSignal<bool>,
+    #[prop(into)] text: Signal<String>,
+    #[prop(default=TooltipDirection::Left)] direction: TooltipDirection,
+    #[prop(optional, into)] is_hidden: Option<Signal<bool>>,
     children: Children
 ) -> impl IntoView {
+
+    let is_hidden = move || is_hidden.as_ref().map(|is_hidden| is_hidden.get()).unwrap_or(false);
+
     view! {
-        <div class="tooltip is-left" class=("is-active", move || visible.get())>
-            <div class="tooltip-container">
-                <div class="tooltip-content p-1 is-size-7 has-text-weight-medium">
-                    { children() }
+        <div class=format!("tooltip {}", direction.as_class())>
+            <div class="tooltip-trigger">
+                { children() }
+            </div>
+            <div class="tooltip-container" style=move || if is_hidden() { "display: none" } else { "" }>
+                <div class="tooltip-content">
+                    <div class="tooltip-item">
+                        { text }
+                    </div>
                 </div>
             </div>
         </div>
