6ef94b56ac257507679c6ae543f3b1d73d89225a
Jessica Schach <jessica.schach@mercedes-benz.com>
2025-11-17 16:56:13 +0100

    LEA: Replaced the `info_text` with a `Tooltip` to the `SaveClusterButton`, when the updating of a cluster is disabled.

--- opendut-lea/src/clusters/configurator/components/controls.rs
+++ opendut-lea/src/clusters/configurator/components/controls.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use leptos::prelude::*;
 use leptos_router::hooks::use_navigate;
 use tracing::{debug, error};
-
+use opendut_lea_components::tooltip::{Tooltip, TooltipDirection};
 use opendut_model::cluster::ClusterDescriptor;
 
 use crate::app::use_app_globals;
@@ -14,25 +14,17 @@ use crate::components::{ButtonColor, ButtonSize, ButtonState, FontAwesomeIcon, I
 use crate::routing::{navigate_to, WellKnownRoutes};
 
 #[component]
-pub fn Controls(cluster_descriptor: ReadSignal<UserClusterDescriptor>, deployed_signal: Signal<IsDeployed>) -> impl IntoView {
-
-    let info_text =
-        Signal::derive(move || {
-            if deployed_signal.get().0 {
-                String::from("Cluster can not be updated or deleted while it is deployed.")
-            } else {
-                String::new()
-            }
-        });
+pub fn Controls(
+    cluster_descriptor: ReadSignal<UserClusterDescriptor>,
+    deployed_signal: Signal<IsDeployed>
+) -> impl IntoView {
 
     let cluster_id = Signal::derive(move || {
         cluster_descriptor.get().id
     });
 
     let use_navigate = use_navigate();
-    let on_delete = {
-        let use_navigate = use_navigate.clone();
-        move || {
+    let on_delete = { move || {
             navigate_to(WellKnownRoutes::ClustersOverview, use_navigate.clone());
         }
     };
@@ -55,7 +47,10 @@ pub fn Controls(cluster_descriptor: ReadSignal<UserClusterDescriptor>, deployed_
 }
 
 #[component]
-fn SaveClusterButton(cluster_descriptor: ReadSignal<UserClusterDescriptor>, deployed_signal: Signal<IsDeployed>) -> impl IntoView {
+fn SaveClusterButton(
+    cluster_descriptor: ReadSignal<UserClusterDescriptor>,
+    deployed_signal: Signal<IsDeployed>
+) -> impl IntoView {
 
     let globals = use_app_globals();
     let toaster = use_toaster();
@@ -117,14 +112,24 @@ fn SaveClusterButton(cluster_descriptor: ReadSignal<UserClusterDescriptor>, depl
         })
     };
 
+    let hide_tooltip = Signal::derive(move || {
+        !deployed_signal.get().0
+    });
+
     view! {
-        <IconButton
-            icon=FontAwesomeIcon::Save
-            color=ButtonColor::Info
-            size=ButtonSize::Normal
-            state=button_state
-            label="Save Cluster"
-            on_action
-        />
+        <Tooltip
+            text="Cluster can not be updated while it is deployed."
+            direction=TooltipDirection::Right
+            is_hidden=hide_tooltip
+        >
+            <IconButton
+                icon=FontAwesomeIcon::Save
+                color=ButtonColor::Info
+                size=ButtonSize::Normal
+                state=button_state
+                label="Save Cluster"
+                on_action
+            />
+        </Tooltip>
     }
 }
