25031d1fd72a8aa2e78fc347839ec819b13f8249
Jessica Schach <jessica.schach@mercedes-benz.com>
2025-11-26 16:27:13 +0100

    Implemented `TestsOverview` page.

--- opendut-lea/src/tests/overview/mod.rs
+++ opendut-lea/src/tests/overview/mod.rs
@@ -1,13 +1,92 @@
 mod row;
 
 use leptos::prelude::*;
+use opendut_lea_components::{BasePageContainer, Breadcrumb, LoadingSpinner};
+use opendut_model::viper::ViperRunDescriptor;
+use crate::app::use_app_globals;
+use crate::tests::overview::row::Row;
 
 #[component(transparent)]
 pub fn TestsOverview() -> impl IntoView {
-    
+
+    let globals = use_app_globals();
+    let refetch_registered_tests = RwSignal::new(());
+
+    let registered_tests: LocalResource<Vec<ViperRunDescriptor>> = {
+        let carl = globals.client.clone();
+
+        LocalResource::new(move || {
+            refetch_registered_tests.track();
+
+            let mut carl = carl.clone();
+
+            async move {
+                carl.viper.list_viper_run_descriptors().await
+                    .expect("Failed to request the list of tests / run descriptors.")
+            }
+        })
+    };
+
+    let tests_table_rows = LocalResource::new(move || async move {
+        let registered_tests = registered_tests.await;
+
+        // Todo: Sort registered_tests.
+
+        registered_tests
+    });
+
+    let breadcrumbs = vec![
+        Breadcrumb::new("Dashboard", "/"),
+        Breadcrumb::new("Tests", "/tests")
+    ];
+
     view! {
-        <div>
-            Tests Overview
-        </div>
+        <BasePageContainer
+            title="Tests"
+            breadcrumbs
+            controls=view! {} // Todo: CreateTestButton
+        >
+        <div class="mt-4">
+                <Transition
+                    fallback=LoadingSpinner
+                >
+                    {move || {
+                        Suspend::new(async move {
+                            let tests_table_rows = tests_table_rows.await;
+
+                            view! {
+                                <table class="table is-hoverable is-fullwidth">
+                                    <thead>
+                                        <tr>
+                                            <th>"Name"</th>
+                                            <th>"Source"</th>
+                                            <th>"Suite"</th>
+                                            <th class="is-narrow has-text-centered">"Action"</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <For
+                                            each = move || tests_table_rows.clone()
+                                            key = |test| test.id
+                                            children = { move |test_descriptor| {
+                                                let on_delete = move || {
+                                                    refetch_registered_tests.notify();
+                                                };
+                                                view! {
+                                                    <Row
+                                                        test_descriptor=RwSignal::new(test_descriptor)
+                                                        on_delete
+                                                    />
+                                                }
+                                            }}
+                                        />
+                                    </tbody>
+                                </table>
+                            }
+                        })
+                    }}
+                </Transition>
+            </div>
+        </BasePageContainer>
     }
 }
---
+++ opendut-lea/src/tests/overview/row.rs
@@ -0,0 +1,91 @@
+use leptos::html::Div;
+use leptos::prelude::*;
+use leptos_use::on_click_outside;
+use opendut_model::viper::ViperRunDescriptor;
+use crate::app::use_app_globals;
+
+#[component]
+pub(crate) fn Row<OnDeleteFn>(
+    test_descriptor: RwSignal<ViperRunDescriptor>,
+    on_delete: OnDeleteFn,
+) -> impl IntoView
+where OnDeleteFn: Fn() + Copy + Send + 'static, {
+
+    let globals = use_app_globals();
+
+    let test_id = create_read_slice(test_descriptor,
+        |test_descriptor| {
+            test_descriptor.id
+        }
+    );
+
+    let test_name = create_read_slice(test_descriptor,
+        |test_descriptor| {
+            test_descriptor.id.to_string() // Todo: Replace by name
+        }
+    );
+
+    let test_source_id = create_read_slice(test_descriptor,
+        |test_descriptor| {
+            test_descriptor.source
+        }
+    );
+
+    let test_source_descriptor = LocalResource::new(move || {
+        let mut carl = globals.client.clone();
+        let source_id = test_source_id.get();
+
+        async move {
+            carl.viper.get_viper_source_descriptor(source_id).await
+                .expect("Failed to request the viper source by id.")
+        }
+    });
+
+    let test_source = Signal::derive(move || {
+        test_source_descriptor.read().as_ref()
+            .map(|descriptor| (descriptor.name.to_string(), descriptor.url.to_string()))
+    });
+
+    let test_suite = create_read_slice(test_descriptor,
+        |test_descriptor| {
+            test_descriptor.suite.to_string()
+        }
+    );
+
+    let configurator_href = move || { format!("/tests/{}/configure/general", test_id.get()) };
+
+    let dropdown_active = RwSignal::new(false);
+    let dropdown = NodeRef::<Div>::new();
+
+    let _ = on_click_outside(dropdown, move |_| dropdown_active.set(false) );
+
+    view! {
+        <tr>
+            <td class="is-vcentered">
+                <a href=configurator_href> { test_name } </a>
+            </td>
+            { move ||
+                test_source.get().map(|(source_name, source_url)| {
+                    view! {
+                        <td class="is-vcentered">
+                            <a href=source_url> { source_name } </a>
+                        </td>
+                    }
+                })
+            }
+            <td class="is-vcentered">
+                <p> { test_suite } </p>
+            </td>
+            <td class="is-vcentered is-flex is-justify-content-center">
+                <div class="is-pulled-right">
+                    // Todo: Implement component `DeleteTestButton`
+                    // <DeleteTestButton
+                    //     test_id
+                    //     button_color=ButtonColor::TextDanger
+                    //     on_delete
+                    // />
+                </div>
+            </td>
+        </tr>
+    }
+}
