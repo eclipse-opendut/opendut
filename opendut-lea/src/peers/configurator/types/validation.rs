9c471142e8deb2988aa69073f8059594c6e15c55
Jessica Schach <jessica.schach@mercedes-benz.com>
2025-12-10 17:25:25 +0100

    LEA (`PeerConfigurator`): Use `Tabs` component for displaying tabs.

--- opendut-lea/src/peers/configurator/types/validation.rs
+++ opendut-lea/src/peers/configurator/types/validation.rs
@@ -16,43 +16,41 @@ impl UserPeerConfiguration {
 
     pub fn valid_devices_tab(&self) -> bool {
         self.devices.iter().all(|device_configuration| {
-            device_configuration.with(|device_configuration| {
-                device_configuration.name.is_right()
-                    && device_configuration.interface.is_some()
-            })
+            let device_configuration = device_configuration.get();
+            device_configuration.name.is_right()
+                && device_configuration.interface.is_some()
         })
     }
 
     pub fn valid_executor_tab(&self) -> bool {
         self.executors.iter().all(|executor| {
-            executor.with(|executor| {
-                let UserPeerExecutor { id: _, kind, results_url, is_collapsed: _ } = executor;
+            let executor = executor.get();
+            let UserPeerExecutor { id: _, kind, results_url, is_collapsed: _ } = executor;
 
-                let kind_is_valid = match kind {
-                    UserPeerExecutorKind::Container {
-                        engine: _,
-                        name,
-                        image,
-                        volumes,
-                        devices,
-                        envs,
-                        ports,
-                        command,
-                        args,
-                    } => {
-                        name.is_right()
-                            && image.is_right()
-                            && volumes.iter().all(|volume| volume.with(|volume| volume.is_right()))
-                            && devices.iter().all(|device| device.with(|device| device.is_right()))
-                            && envs.iter().all(|env| env.with(|env| env.name.is_right()))
-                            && ports.iter().all(|port| port.with(|port| port.is_right()))
-                            && command.is_right()
-                            && args.iter().all(|arg| arg.with(|arg| arg.is_right()))
-                    }
-                };
+            let kind_is_valid = match kind {
+                UserPeerExecutorKind::Container {
+                    engine: _,
+                    name,
+                    image,
+                    volumes,
+                    devices,
+                    envs,
+                    ports,
+                    command,
+                    args,
+                } => {
+                    name.is_right()
+                        && image.is_right()
+                        && volumes.iter().all(|volume| volume.get().is_right())
+                        && devices.iter().all(|device| device.get().is_right())
+                        && envs.iter().all(|env| env.get().name.is_right())
+                        && ports.iter().all(|port| port.get().is_right())
+                        && command.is_right()
+                        && args.iter().all(|arg| arg.get().is_right())
+                }
+            };
 
-                kind_is_valid && results_url.is_right()
-            })
+            kind_is_valid && results_url.is_right()
         })
     }
 }
