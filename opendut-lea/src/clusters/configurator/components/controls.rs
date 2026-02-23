e88824f71b473560abf8342ce74a25d68da9340d
Aung Phone Khant <morriszin016@gmail.com>
2026-02-23 03:22:49 +0000

    LEA: Add deploy toogle to ClusterConfigurator

--- opendut-lea/src/clusters/configurator/components/controls.rs
+++ opendut-lea/src/clusters/configurator/components/controls.rs
@@ -3,7 +3,11 @@ use std::sync::Arc;
 use leptos::prelude::*;
 use leptos_router::hooks::use_navigate;
 use tracing::{debug, error};
+use opendut_carl_api::carl::ClientError;
+use opendut_carl_api::carl::cluster::StoreClusterDeploymentError;
 use opendut_lea_components::tooltip::{Tooltip, TooltipDirection};
+use opendut_lea_components::Toggle;
+use opendut_model::cluster::ClusterDeployment;
 use opendut_model::cluster::ClusterDescriptor;
 use opendut_model::cluster::state::ClusterState;
 
@@ -20,10 +24,95 @@ pub fn Controls(
     cluster_descriptor: ReadSignal<UserClusterDescriptor>,
     deployed_signal: Signal<IsDeployed>,
     cluster_state: Signal<ClusterState>,
+    refetch_cluster_deployments: RwSignal<()>
 ) -> impl IntoView {
 
     let cluster_id = Signal::derive(move || {
         cluster_descriptor.get().id
+    });
+    
+    let globals = use_app_globals();
+    let toaster = use_toaster();
+
+    let on_deploy = {
+        let carl = globals.client.clone();
+        let toaster = Arc::clone(&toaster);
+
+        move || {
+            let mut carl = carl.clone();
+            let toaster = Arc::clone(&toaster);
+            let cluster_id = cluster_id.get_untracked();
+
+            leptos::task::spawn_local(async move {
+                match carl.cluster.store_cluster_deployment(ClusterDeployment { id: cluster_id }).await {
+                    Ok(cluster_id) => {
+                        debug!("Successfully stored cluster deployment: {}", cluster_id);
+                        toaster.toast(
+                            Toast::builder()
+                                .simple("Successfully stored cluster deployment!")
+                                .success()
+                        );
+                    }
+                    Err(cause) => {
+                        error!("Failed to store cluster deployment <{}>, due to error: {:?}", cluster_id, cause);
+                        match cause {
+                            ClientError::UsageError(StoreClusterDeploymentError::IllegalPeerState { invalid_peers, .. }) => {
+                                toaster.toast(
+                                    Toast::builder()
+                                        .simple(format!("Failed to store cluster deployment! Peers already in use: {}", invalid_peers.iter().map(|peer| peer.to_string()).collect::<Vec<_>>().join(", ")))
+                                        .error()
+                                );
+                            }
+                            _ => {
+                                toaster.toast(
+                                    Toast::builder()
+                                        .simple("Failed to store cluster deployment!")
+                                        .error()
+                                );
+                            }
+                        };
+                    }
+                }
+                refetch_cluster_deployments.notify();
+            })
+        }
+    };
+
+    let on_undeploy = {
+        let carl = globals.client.clone();
+        let toaster = Arc::clone(&toaster);
+
+        move || {
+            let mut carl = carl.clone();
+            let toaster = Arc::clone(&toaster);
+            let cluster_id = cluster_id.get_untracked();
+
+            leptos::task::spawn_local(async move {
+                match carl.cluster.delete_cluster_deployment(cluster_id).await {
+                    Ok(_) => {
+                        toaster.toast(Toast::builder()
+                            .simple("Successfully deleted cluster deployment!")
+                            .success()
+                        );
+                    }
+                    Err(_) => {
+                        toaster.toast(Toast::builder()
+                            .simple("Failed to delete cluster deployment!")
+                            .error()
+                        );
+                    }
+                }
+                refetch_cluster_deployments.notify();
+            })
+        }
+    };
+
+    let deploy_tooltip_text = Signal::derive(move || {
+        if deployed_signal.get().0 {
+            "Deployment requested".to_string()
+        } else {
+            "Undeployed".to_string()
+        }
     });
 
     let use_navigate = use_navigate();
@@ -34,6 +123,15 @@ pub fn Controls(
 
     view! {
         <div class="is-flex is-align-items-center">
+            <Tooltip text=deploy_tooltip_text>     
+                <Toggle
+                    is_active=Signal::derive(move || deployed_signal.get().0)
+                    on_action=move || {
+                        if deployed_signal.get().0 { on_undeploy() } else { on_deploy() }
+                    }
+                />
+            </Tooltip>
+            <div class="px-2" />
             <ClusterHealth state=cluster_state />
             <div class="px-2" />
             <SaveClusterButton
