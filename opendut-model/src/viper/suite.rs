fb8c1924a9ab7166508a3723ebfacc4451c98b84
Frank Märkle <Frank.Maerkle@mercedes-benz.com>
2026-04-19 07:49:45 +0200

    Model: Add documentation to clarify `ViperTestSuiteDescriptor` vs. `ViperTestRunDescriptor`.

--- opendut-model/src/viper/suite.rs
+++ opendut-model/src/viper/suite.rs
@@ -1,9 +1,12 @@
 use crate::viper::{ViperSourceId, ViperTestSuiteIdentifier};
 
 
+/// Meta-information about a VIPER test suite,
+/// including constraints for how it can be parametrized.
 #[derive(Clone, Debug)]
 pub struct ViperTestSuiteDescriptor {
     pub id: ViperTestSuiteIdentifier,
     pub source: ViperSourceId,
+    /// Constraints for how the parameter values may look like.
     pub parameters: super::ViperParameterDescriptors,
 }
