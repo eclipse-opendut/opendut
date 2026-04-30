5bb1770de5ddf9e3f9ffa93bcdce30efc20d0e36
sdv-infy <sdv_infy@infosys.com>
2026-04-30 12:05:36 +0530

    fix issue 431

--- opendut-lea/src/peers/configurator/tabs/setup/mod.rs
+++ opendut-lea/src/peers/configurator/tabs/setup/mod.rs
@@ -1,3 +1,4 @@
+use leptos::either::Either;
 use leptos::prelude::*;
 
 use crate::components::WarningMessage;
@@ -7,14 +8,27 @@ use crate::peers::configurator::types::UserPeerConfiguration;
 #[component]
 pub fn SetupTab(peer_configuration: ReadSignal<UserPeerConfiguration>) -> impl IntoView {
 
+    let is_new = Signal::derive(move || peer_configuration.get().is_new);
     let kind = Signal::derive(move || GenerateSetupStringKind::Edgar(peer_configuration.get().id));
 
     view! {
         <div class="field">
-            <GenerateSetupStringForm kind />
-            <WarningMessage>
-                Setup-Strings may only be used to set up one host. For setting up multiple hosts, you should create a peer for each host.
-            </WarningMessage>
+            {move || {
+                if is_new.get() {
+                    Either::Left(view! {
+                        <WarningMessage>
+                            "The peer configuration must be saved before a Setup-String can be generated. Please complete the configuration and save the peer first."
+                        </WarningMessage>
+                    })
+                } else {
+                    Either::Right(view! {
+                        <GenerateSetupStringForm kind />
+                        <WarningMessage>
+                            "Setup-Strings may only be used to set up one host. For setting up multiple hosts, you should create a peer for each host."
+                        </WarningMessage>
+                    })
+                }
+            }}
         </div>
     }
 }
