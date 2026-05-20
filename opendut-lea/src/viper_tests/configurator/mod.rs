b4d155d1ec6622c4de95e4c3ce36aad245ac8b13
Jessica Schach <jessica.schach@mercedes-benz.com>
2026-05-20 18:17:46 +0200

    LEA (`ViperTestConfigurator`): Set initial value of all `BooleanParameter` to `None`.

--- opendut-lea/src/viper_tests/configurator/mod.rs
+++ opendut-lea/src/viper_tests/configurator/mod.rs
@@ -3,7 +3,7 @@ use leptos::prelude::*;
 use leptos_router::hooks::{use_navigate, use_params_map};
 use opendut_lea_components::{BasePageContainer, Breadcrumb, LoadingSpinner, UserInputError, UserInputValue};
 use opendut_lea_components::tabs::{Tab, Tabs};
-use opendut_model::viper::{ViperBindingValue, ViperParameterDescriptor, ViperParameterName, ViperTestId, ViperTestRunDescriptor};
+use opendut_model::viper::{ViperParameterDescriptor, ViperParameterName, ViperTestId, ViperTestRunDescriptor};
 use crate::app::use_app_globals;
 use crate::components::use_active_tab;
 use crate::routing::{navigate_to, WellKnownRoutes};
@@ -115,9 +115,9 @@ pub fn ViperTestConfigurator() -> impl IntoView {
                                 .cloned()
                                 .unwrap_or_else(|| {
                                     match parameter {
-                                        ViperParameterDescriptor::BooleanParameter { default, .. } => {
-                                            let default = default.unwrap_or(false);
-                                            ViperBindingValueInput::Right(Some(ViperBindingValue::BooleanValue(default)))
+                                        ViperParameterDescriptor::BooleanParameter { .. } => {
+                                            // Todo: Remove this match arm, when backend/VIPER forces default parameters for `BooleanParameter`.
+                                            ViperBindingValueInput::Right(None)
                                         }
                                         _ if parameter.has_default_value() => {
                                             ViperBindingValueInput::Right(None)
