899b0f1e88d5f5973133c87d67bb2bc3e97dee23
Frank Märkle <Frank.Maerkle@mercedes-benz.com>
2026-07-05 21:22:52 +0200

    CARL ResourceManager: Initial prototype for subscribing to multiple resources.

--- opendut-carl/src/resource/manager/mod.rs
+++ opendut-carl/src/resource/manager/mod.rs
@@ -156,6 +156,82 @@ impl ResourceManager {
         });
     }
 
+
+    //TODO move implementation into submodule
+    pub async fn spawn_event_listener_aggregate_2<R1: Resource + Subscribable, R2: Resource + Subscribable>(
+        &self,
+        on_event: impl AsyncFn((Option<SubscriptionEvent<R1>>, Option<SubscriptionEvent<R2>>)) + Send + 'static,
+    ) {
+
+        let mut subscription1 = self.subscribe::<R1>().await;
+        let mut subscription2 = self.subscribe::<R2>().await;
+
+        let aggregate_resource_name = format!("({}, {})", R1::type_name(), R2::type_name());
+
+
+        let cancel = self.event_listener_cancel.clone();
+
+
+        enum Abort { Yes, No }
+
+        async fn receive<R: Resource>(subscription: &mut Subscription<R>) -> Result<SubscriptionEvent<R>, Abort> { //TODO maybe pull out and re-use for non-aggregate as well
+            let resource_name = R::type_name();
+
+            match subscription.receive().await {
+                Ok(event) => {
+                    Ok(event) //TODO simplify
+                }
+                Err(subscription::ReceiveError::Broadcast(error)) => match error {
+                    tokio::sync::broadcast::error::RecvError::Closed => {
+                        debug!("ResourceManager subscription channel for {resource_name} closed. Aborting."); //TODO mention aggregate channel name
+                        Err(Abort::Yes)
+                    }
+                    tokio::sync::broadcast::error::RecvError::Lagged(skipped_messages) => {
+                        warn!("ResourceManager subscription channel for {resource_name} lagged behind and had to skip {skipped_messages} messages.");
+                        Err(Abort::No)
+                    },
+                }
+            }
+        }
+
+
+        tokio::task::spawn_blocking(move || {
+            tokio::runtime::Handle::current().block_on(async move {
+                let mut previous1 = None;
+                let mut previous2 = None;
+
+                loop {
+                    tokio::select! {
+                        result = receive(&mut subscription1) => match result {
+                            Ok(event) => {
+                                previous1.replace(event);
+
+                                let notification = (previous1.clone(), previous2.clone());
+                                on_event(notification).await;
+                            }
+                            Err(Abort::Yes) => break,
+                            Err(Abort::No) => {}, //do nothing
+                        },
+                        result = receive(&mut subscription2) => match result {
+                            Ok(event) => {
+                                previous2.replace(event);  //TODO pull into closure?
+
+                                let notification = (previous1.clone(), previous2.clone());
+                                on_event(notification).await;
+                            }
+                            Err(Abort::Yes) => break,
+                            Err(Abort::No) => {}, //do nothing
+                        },
+                        _ = cancel.cancelled() => {
+                            debug!("ResourceManager is cancelling subscription channel for {aggregate_resource_name}.");
+                            break;
+                        }
+                    }
+                }
+            })
+        });
+    }
+
     /// Get events when a resource is inserted/updated or removed.
     ///
     /// For an easier to use interface, see: [ResourceManager::spawn_event_listener]
@@ -279,6 +355,7 @@ mod test {
     use std::vec;
 
     use googletest::prelude::*;
+    use opendut_model::peer::state::PeerConnectionState;
     use tokio::sync::mpsc;
 
     use super::*;
@@ -386,6 +463,55 @@ mod test {
         ).await?;
 
         assert!(received.is_some());
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn should_spawn_an_event_listener_that_aggregates_two_resources() -> Result<()> {
+        let (testee, _cancel) = ResourceManager::new_in_memory();
+
+        let (sender, mut receiver) = mpsc::channel(1);
+
+        testee.spawn_event_listener_aggregate_2::<ClusterDeployment, PeerConnectionState>(
+            async move |event| {
+                sender.send(event).await.unwrap();
+            }
+        ).await;
+
+
+
+        let id1 = ClusterId::random();
+        let resource1 = ClusterDeployment { id: id1 };
+        testee.insert(id1, resource1.clone()).await?;
+
+        {
+            let received = tokio::time::timeout(
+                Duration::from_secs(5),
+                receiver.recv()
+            ).await?;
+
+            let (received1, received2) = received.unwrap();
+
+            assert_eq!(received1, Some(SubscriptionEvent::Inserted { id: id1, value: resource1.clone() }));
+            assert_eq!(received2, None);
+        }
+
+
+        let id2 = PeerId::random();
+        let resource2 = PeerConnectionState::Offline;
+        testee.insert(id2, resource2.clone()).await?;
+
+        {
+            let received = tokio::time::timeout(
+                Duration::from_secs(5),
+                receiver.recv()
+            ).await?;
+
+            let (received1, received2) = received.unwrap();
+
+            assert_eq!(received1, Some(SubscriptionEvent::Inserted { id: id1, value: resource1 }));
+            assert_eq!(received2, Some(SubscriptionEvent::Inserted { id: id2, value: resource2 }));
+        }
         Ok(())
     }
 }
