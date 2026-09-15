1c9013f3e3318455a58c3bd98aa22ed3508edbc0
Reimar Stier <reimar.stier@mercedes-benz.com>
2026-09-15 09:46:54 +0200

    Chore → Fix check of NetBird management URL. A URL that returns a 2xx code is required, using the instance status endpoint of NetBird management.

--- .ci/deploy/localenv/data/carl/entrypoint.sh
+++ .ci/deploy/localenv/data/carl/entrypoint.sh
@@ -8,8 +8,8 @@ echo "OpenDuT-CARL init script for local test environment"
 wait_for_url "$KEYCLOAK_URL" 600 5 || exit 1
 # wait for realm to be provisioned
 wait_for_url "$KEYCLOAK_URL/realms/netbird/.well-known/openid-configuration" 600 5 || exit 1
-# wait for netbird to be ready
-wait_for_url "$NETBIRD_MANAGEMENT_URL" 600 5 || exit 1
+# wait for netbird to be ready, see https://docs.netbird.io/api/resources/instance#get-instance-status
+wait_for_url "$NETBIRD_MANAGEMENT_URL/api/instance" 600 5 || exit 1
 # wait for service response
 wait_for_netbird_user_to_be_synced_from_keycloak "netbird" 600 5 || exit 1
 
